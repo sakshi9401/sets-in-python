810a38392273c59d62da8cdee286bc8c26c8f8eb
sakshi9401 <68016527+sakshi9401@users.noreply.github.com>
2020-07-17 13:29:24 +0530

    Add files via upload

---
+++ set example2.py
@@ -0,0 +1,21 @@
+l = {1 , 2, 3, 4, 5}
+
+s_of_set = set(l)
+
+x = l.pop()
+
+print(x)
+
+sset = {"apple", "banana", "cherry"}
+
+sset.remove("banana")
+
+print(sset)
+
+l.clear()
+
+print(l)
+
+print(s_of_set)
+
+print(type(s_of_set))
